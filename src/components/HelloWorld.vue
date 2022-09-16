014f9a44eb595f9022d458be2c8ff59940af542c
RobinWM <302740969@qq.com>
2022-09-16 18:18:35 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,13 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <RBTable :columns="columns" :tableData="tableData">
+      <span slot="behind">
+        <el-table-column prop="behind" label="前列" width="180"/>
+      </span>
+      <span slot="before">
+        <el-table-column prop="before" label="后列" width="180"/>
+      </span>
+    </RBTable>
   </div>
 </template>
 
@@ -35,6 +16,29 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      columns: [
+        {
+          label: '测试1',
+          prop: 'test1',
+        },
+        {
+          label: '测试2',
+          prop: 'test2',
+          width: 200
+        }
+      ],
+      tableData: [
+        {
+          test1: 'test1',
+          test2: 'test2',
+          behind: '前列',
+          before: '2222222222'
+        }
+      ]
+    }
   }
 }
 </script>
@@ -44,14 +48,17 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './plugins/element.js'
+
+import RBComponents from 'rb-components-vue'
+import 'rb-components-vue/lib/rb-components-vue.css'
+
+Vue.use(RBComponents)
 
 Vue.config.productionTip = false
 
